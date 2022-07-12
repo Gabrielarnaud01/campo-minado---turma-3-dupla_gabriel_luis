73f745cfebe46f4ba7727297494819495ed14868
Luis Candido <103050923+luiseduardofc1500@users.noreply.github.com>
2022-07-11 22:01:03 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -24,54 +24,45 @@ adjacentes.
 bal
 */
 
+typedef struct{
+    int tipo;
+    int n_minas;
+}espaco;
 
 
-int main(){
-    //matriz do jogo
-    int **matriz;
-    int *vet_rand;
-
-    vet_rand = malloc(sizeof(int)*40);
-    matriz = malloc(sizeof(int*)*10);
-    srand(time(0));
-
-    for(int i =0;i<40;i++){
-      vet_rand[i] = rand() % 200 + 1;
+void cria_campo(int *v_rand, espaco **matriz){
+     for(int i =0;i<40;i++){
+      v_rand[i] = rand() % 200 + 1;
       for(int j = 0;j<i;j++){
-        if(i>0 && vet_rand[j] == vet_rand[i]){
-          vet_rand[i] = rand() % 200 + 1;
+        if(i>0 && v_rand[j] == v_rand[i]){
+          v_rand[i] = rand() % 200 + 1;
           j--;
-          
         }
       }
-    }
-
-    for(int i=0;i<10;i++){
-        matriz[i] = malloc(sizeof(int*)*20);
     }
     for(int i=0;i<10;i++){
         for(int j=0;j<20;j++){
             if(i==0){
-              matriz[i][j] = i+j + 1;
+              matriz[i][j].tipo = i+j + 1;
               for(int k=0;k<40;k++){
-                if(matriz[i][j] == vet_rand[k]){
-                  matriz[i][j] =4001;
+                if(matriz[i][j].tipo == v_rand[k]){
+                  matriz[i][j].tipo = 1;
                 }
               }
             }
             if(i>0 && j==0){
-              matriz[i][j] = i*20 +1;
+              matriz[i][j].tipo = i*20 +1;
               for(int k=0;k<40;k++){
-                if(matriz[i][j] == vet_rand[k]){
-                  matriz[i][j] =4001;
+                if(matriz[i][j].tipo == v_rand[k]){
+                  matriz[i][j].tipo = 1;
                 }
               }
             }
             if(i>0 && j>0){
-              matriz[i][j] = i*20 +j +1;
+              matriz[i][j].tipo = i*20 +j +1;
               for(int k=0;k<40;k++){
-                if(matriz[i][j] == vet_rand[k]){
-                  matriz[i][j] =4001;
+                if(matriz[i][j].tipo == v_rand[k]){
+                  matriz[i][j].tipo = 1;
                 }
               }
             }
@@ -79,25 +70,47 @@ int main(){
     }
     for(int i=0;i<10;i++){
         for(int j=0;j<20;j++){
-          if(matriz[i][j]!=4001){
-            matriz[i][j]=0;
+          if(matriz[i][j].tipo!=1){
+            matriz[i][j].tipo = 0;
           }
         }
     }
+}
+
+
+int main(){
+    //matriz do jogo
+    espaco **matriz;
+    int *vet_rand;
+
+    vet_rand = malloc(sizeof(int)*40);
+    matriz = malloc(sizeof(espaco*)*10);
+    srand(time(0));
+
+    for(int i=0;i<10;i++){
+        matriz[i] = malloc(sizeof(espaco*)*20);
+    }
+    cria_campo(vet_rand,matriz);
+    // func para checar numero de bombas.
+
+
     for(int i=0;i<10;i++){
         for(int j=0;j<20;j++){
-            printf("%d ",matriz[i][j]);
+            printf("%d ",matriz[i][j].tipo);
         }
         printf("\n");
     }
-printf("\n");
-printf("\n");
-    //  Podemos atribuir o valor  0 às celular que não tiverem nem numero nem bomba, atribuiremos a quantidade de bombas->
-    //nas celulas adjascentes a celulas que tiverem bombas adjascentes e na celula das bombas podemos adicionar um numero maior de 40;
-    //  Para a atribuição das bombas nos podemos gerar  40 numeros aleatorios entre 0-199 e colocar bombas nas posiçôes geradas!!
-    //  Para a verificação de bombas ao redor como usaremos a heap//alocação precisaremos checar a casa -11,-10,-9,-1,+1,+9,+10 e +11 de cada celula->
-    //e ai atribuir o numero de quantidade de bombas a essa celula!!
+    printf("\n");
+    printf("\n");
+/*
+    Podemos atribuir o valor  0 às celular que não tiverem nem numero nem bomba, atribuiremos a quantidade de bomba nas celulas adjascentes 
+    e na celula das bombas podemos adicionar um numero maior de 40;
 
+    Para a atribuição das bombas nos podemos gerar  40 numeros aleatorios entre 0-199 e colocar bombas nas posiçôes geradas!!
 
+    Para a verificação de bombas ao redor como usaremos a heap//alocação precisaremos checar a casa -11,-10,-9,-1,+1,+9,+10 e +11 
+    de cada celula e ai atribuir o numero de quantidade de bombas a essa celula!!
+
+*/
     return 0; 
 }
